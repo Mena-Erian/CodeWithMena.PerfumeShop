b3a0cdeb74fcac2c051a446b61078a5eb60eb646
Mena Erian <mena3koko2karas1@gmail.com>
2025-11-17 20:14:40 +0200

    chore: Edit and Delete

--- CodeWithMena.PerfumeShop.PL/Controllers/PerfumesOilController.cs
+++ CodeWithMena.PerfumeShop.PL/Controllers/PerfumesOilController.cs
@@ -1,8 +1,11 @@
 ﻿using CodeWithMena.PerfumeShop.BLL.Contracts;
+using CodeWithMena.PerfumeShop.DAL.Common.Entities;
+using CodeWithMena.PerfumeShop.DAL.Common.Enums;
 using CodeWithMena.PerfumeShop.DAL.Entities;
 using CodeWithMena.PerfumeShop.PL.ViewModels.Perfumes;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace CodeWithMena.PerfumeShop.PL.Controllers
 {
@@ -59,14 +62,64 @@ namespace CodeWithMena.PerfumeShop.PL.Controllers
 
             if (perfume == null) return BadRequest();
 
-            return View(perfume);
+            var viewModel = new PerfumeOilEditDto()
+            {
+                Id = perfume.Id,
+                Name = perfume.Name,
+                Description = perfume.Description,
+                PerfumePrice = perfume.PerfumePrice,
+                AvailableQuantityPerGram = perfume.AvailableQuantityPerGram,
+                FragranceType = perfume.FragranceType,
+                FragranceFamily = perfume.FragranceFamily,
+                RatingOfSale = perfume.RatingOfSale,
+
+            };
+
+
+            TempData["PerfumeOilId"] = perfume.Id;
+
+            return View(viewModel);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(PerfumeOilEditDto model)
+        {
+            if (model.Id.ToString() != TempData["PerfumeOilId"]?.ToString())
+                return BadRequest();
+
+            if (!ModelState.IsValid) return View(model);
+
+            var perfume = await perfumeOilService.GetPerfumeOilByIdAsync(model.Id);
+
+            if (perfume == null) return BadRequest();
+
+            perfume.Name = model.Name;
+            perfume.Description = model.Description;
+            perfume.PerfumePrice = model.PerfumePrice;
+            perfume.AvailableQuantityPerGram = model.AvailableQuantityPerGram;
+            perfume.FragranceType = model.FragranceType;
+            perfume.FragranceFamily = model.FragranceFamily;
+            perfume.RatingOfSale = model.RatingOfSale;
+
+            await perfumeOilService.UpdatePerfumeOilAsync(perfume);
+
+            return RedirectToAction(actionName: nameof(Details), new { perfume.Id, viewName = "Details" });
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> Download()
         {
             var allPerfumes = await perfumeOilService.GetAllPerfumesOilAsync();
 
             return Json(allPerfumes);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            bool isDeleted = await perfumeOilService.DeletePerfumeOilAsync(id);
+            return RedirectToAction(nameof(Index));
         }
 
     }
---
+++ CodeWithMena.PerfumeShop.PL/ViewModels/Perfumes/PerfumeOilEditDto.cs
@@ -0,0 +1,25 @@
+﻿using CodeWithMena.PerfumeShop.DAL.Common.Entities;
+using CodeWithMena.PerfumeShop.DAL.Common.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeWithMena.PerfumeShop.PL.ViewModels.Perfumes
+{
+    public class PerfumeOilEditDto
+    {
+        public required Guid Id { get; set; }
+
+        [MinLength(2)]
+        public required string Name { get; set; }
+
+        public decimal? AvailableQuantityPerGram { get; set; }
+        public string? Description { get; set; }
+        public FragranceType FragranceType { get; set; }
+        public FragranceFamily? FragranceFamily { get; set; }
+
+        [Range(1, 10)]
+        [Display(Name = "Rating Of Sale")]
+        public int RatingOfSale { get; set; } = 0;
+
+        public BasePerfumePrice? PerfumePrice { get; set; }
+    }
+}
